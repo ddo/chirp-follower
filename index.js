5db44a9f9291bfd70a22594db3ed1901192350e9
Ddo <joeddo89@gmail.com>
2014-07-25 17:07:18 +0700

    init

---
+++ index.js
@@ -0,0 +1,74 @@
+var ChirpRest = require('chirp-rest');
+var debug     = require('debug')('chirp-follower');
+
+module.exports = Follower;
+
+function Follower(opt) {
+    if(!(this instanceof Follower)) {
+        return new Follower(opt);
+    }
+
+    if(!(opt.consumer && opt.consumer.public && opt.consumer.secret)) {
+        throw new Error('consumer.public and consumer.secret are required');
+    }
+
+    if(!(opt.token && opt.token.public && opt.token.secret)) {
+        throw new Error('token.public and token.secret are required');
+    }
+
+    this.rest = new ChirpRest({
+        consumer:   opt.consumer,
+        token:      opt.token
+    });
+}
+
+Follower.prototype.follow = function(user, callback) {
+    var self = this;
+
+    var user_id = user;
+
+    if(typeof user === 'object') {
+        user_id = user.id_str
+    }
+
+    debug('#follow', user_id);
+
+    self.rest.post('https://api.twitter.com/1.1/friendships/create.json', {
+        user_id: user_id,
+        follow: true
+    }, function(err, res, body) {
+        if(err) {
+            debug('#follow err', err);
+            return callback(err);
+        }
+
+        debug('#follow success');
+
+        return callback(null, body);
+    });
+};
+
+Follower.prototype.unfollow = function(user, callback) {
+    var self = this;
+
+    var user_id = user;
+
+    if(typeof user === 'object') {
+        user_id = user.id_str
+    }
+
+    debug('#unfollow', user_id);
+
+    self.rest.post('https://api.twitter.com/1.1/friendships/destroy.json', {
+        user_id: user_id,
+    }, function(err, res, body) {
+        if(err) {
+            debug('#unfollow err', err);
+            return callback(err);
+        }
+
+        debug('#unfollow success');
+
+        return callback(null, body);
+    });
+};
